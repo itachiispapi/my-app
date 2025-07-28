86c1b4da3e412cb378b59f5ca98ee84d30fc248f
Afton Goulding <aftografto@gmail.com>
2025-07-28 12:28:17 -0600

    fixed menuitem with dropdown

---
+++ src/FilteredList.jsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Dropdown, DropdownButton } from 'react-bootstrap';
+import List from './List';
+
+class FilteredList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: "",
+      type: "All"
+    };
+  }
+
+  onSearch = (event) => {
+    this.setState({ search: event.target.value.trim().toLowerCase() });
+  }
+
+  onFilter = (type) => {
+    this.setState({ type });
+  }
+
+  filterItem = (item) => {
+    const matchesSearch = item.name.toLowerCase().includes(this.state.search);
+    const matchesType = this.state.type === "All" || item.type === this.state.type;
+    return matchesSearch && matchesType;
+  }
+
+  render() {
+    return (
+      <div className="filter-list">
+        <h1>Produce Search</h1>
+        <DropdownButton id="dropdown-basic-button" title={this.state.type} onSelect={this.onFilter}>
+          <Dropdown.Item eventKey="All">All</Dropdown.Item>
+          <Dropdown.Item eventKey="Fruit">Fruit</Dropdown.Item>
+          <Dropdown.Item eventKey="Vegetables">Vegetables</Dropdown.Item>
+        </DropdownButton>
+
+        <input type="text" placeholder="Search" onChange={this.onSearch} />
+        <List items={this.props.items.filter(this.filterItem)} />
+      </div>
+    );
+  }
+}
+
+export default FilteredList;
